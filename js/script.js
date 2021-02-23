9bffdd09a2d23585f87ca03a1e0d1268771190a6
pcrantfo <pcrantfo@gmail.com>
2021-02-23 12:27:10 -0700

    Create table functions
    Creates two functions that create an html table based on the pokemonList objects. If an object property has multiple array entries, the function will parse and separate by a ", ". See multiple type entries to understand output.

--- js/script.js
+++ js/script.js
@@ -55,4 +55,51 @@ let pokemonList = [
     }
 ];
 
-console.log(pokemonList);
+//  for (let x of pokemonList) {
+//      console.log(x.nationalIndex);
+//      console.log(x.name);
+//      console.log(x.height);
+//      for (let i of x.types) {
+//          console.log(i);
+//      };
+//  };
+
+function generateTableHead(table, data) {
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    for (let key of data) {
+        let th = document.createElement("th");
+        let text = document.createTextNode(key);
+        th.appendChild(text);
+        row.appendChild(th);
+    }
+}
+
+function generateTable(table, data) {
+    for (let element of data) {
+        let row = table.insertRow();
+        for (let key in element) {
+            let cell = row.insertCell();
+            if (element[key].length > 1 && typeof element[key] == 'object') { 
+                let multiEntryArray = ' ';
+                for (let i of element[key]) {
+                    if (i == element[key][element[key].length-1]) {
+                        multiEntryArray += `${i}`;
+                    } else {
+                        multiEntryArray += `${i}, `;
+                    }
+                }
+                let text = document.createTextNode(multiEntryArray);
+                cell.appendChild(text);
+            } else {
+                let text = document.createTextNode(element[key]);
+                cell.appendChild(text);
+            }
+        }
+    }
+}
+
+let table = document.querySelector("table");
+let data = Object.keys(pokemonList[0]);
+generateTable(table, pokemonList);
+generateTableHead(table, data);
