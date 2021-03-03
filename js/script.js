2d018774ab08606addf9e86da2a5a68ceffe4c7a
Peyton Crantford <79102975+pcrantfo@users.noreply.github.com>
2021-03-03 16:33:29 -0700

    pokemonRepository
    create IIFE to store pokemonList array with functions .add and .getAll to modify or access pokemonList.
    
    Update write functions to use pokemonRepository.getAll()

--- js/script.js
+++ js/script.js
@@ -1,59 +1,73 @@
-let pokemonList = [
-    {
-        nationalIndex: 1,
-        name: 'bulbasaur',
-        height: 0.7,
-        types: ['grass', 'poison']
-    },
-    {
-        nationalIndex: 2,
-        name: 'ivysaur',
-        height: 1,
-        types: ['grass', 'poison']
-    },
-    {
-        nationalIndex: 3,
-        name: 'venusaur',
-        height: 2,
-        types: ['grass', 'posion']
-    },
-    {
-        nationalIndex: 4,
-        name: 'charmander',
-        height: 0.6,
-        types: ['fire']
-    },
-    {
-        nationalIndex: 5,
-        name: 'charmeleon',
-        height: 1.1,
-        types: ['fire']
-    },
-    {
-        nationalIndex: 6,
-        name: 'charizard',
-        height: 1.7,
-        types: ['fire', 'flying']
-    },
-    {
-        nationalIndex: 7,
-        name: 'squirtle',
-        height: 0.5,
-        types: ['water']
-    },
-    {
-        nationalIndex: 8,
-        name: 'wartortle',
-        height: 1,
-        types: ['water']
-    },
-    {
-        nationalIndex: 9,
-        name: 'blastoise',
-        height: 1.6,
-        types: ['water']
+// https://pokedex.org/
+
+// create IIFE to store pokemonList array with functions .add and .getAll to modify or access pokemonList
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            nationalIndex: 1,
+            name: 'bulbasaur',
+            height: 0.7,
+            types: ['grass', 'poison']
+        },
+        {
+            nationalIndex: 2,
+            name: 'ivysaur',
+            height: 1,
+            types: ['grass', 'poison']
+        },
+        {
+            nationalIndex: 3,
+            name: 'venusaur',
+            height: 2,
+            types: ['grass', 'posion']
+        },
+        {
+            nationalIndex: 4,
+            name: 'charmander',
+            height: 0.6,
+            types: ['fire']
+        },
+        {
+            nationalIndex: 5,
+            name: 'charmeleon',
+            height: 1.1,
+            types: ['fire']
+        },
+        {
+            nationalIndex: 6,
+            name: 'charizard',
+            height: 1.7,
+            types: ['fire', 'flying']
+        },
+        {
+            nationalIndex: 7,
+            name: 'squirtle',
+            height: 0.5,
+            types: ['water']
+        },
+        {
+            nationalIndex: 8,
+            name: 'wartortle',
+            height: 1,
+            types: ['water']
+        },
+        {
+            nationalIndex: 9,
+            name: 'blastoise',
+            height: 1.6,
+            types: ['water']
+        }
+    ];
+
+    return {
+        add: function(pokemon) {
+        },
+        getAll: function() {
+            return pokemonList;
+        },
     }
-];
+})();
+
 
 // pokemonHeight loops through a list of objects (pokemonList in this case) and returns all pokemon's height value (with dimension), as well as checking if a pokemon is taller than 2m.
 
@@ -99,7 +113,7 @@ function writeHeights (content) {
 // combines three above functions to write a nested div structure displaying all meter-formatted heights of the pokemon in pokemonList
 (function () {
     let results = "";
-    pokemonList.forEach(function(pokedexEntry) {
+    pokemonRepository.getAll().forEach(function(pokedexEntry) {
         results += formatHeightDiv(pokedexEntry);
         return results;
     });
