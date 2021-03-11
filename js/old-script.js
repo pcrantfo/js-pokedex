8c11fa204ac3f1d5219d49aa42faa39aca4a1d56
Peyton Crantford <79102975+pcrantfo@users.noreply.github.com>
2021-03-11 16:25:13 -0700

    Copy of modal code
    For reference when replaced later

--- js/old-script.js
+++ js/old-script.js
@@ -56,6 +56,116 @@
     types: ['water']
 }
 ];
+ *    MODAL CODE: goes inside pokemonRepository
+ *========================**/
+
+//  showDetails: function(pokedexEntry) {
+//     pokemonRepository.loadDetails(pokedexEntry).then(function () {
+//         console.log(pokedexEntry);
+
+//         let modalContainer = document.createElement('div');
+//         modalContainer.classList.add('modal-container');
+//         modalContainer.innerHTML = '';
+
+//         let modal = document.createElement('div');
+//         modal.classList.add('modal');
+
+//         let modalImage = document.createElement('img');
+//         modalImage.classList.add('modal-image');
+//         modalImage.src = pokedexEntry.imageUrl;
+
+//         let modalInteriorDiv = document.createElement('div');
+//         modalInteriorDiv.classList.add('modal-interior-div');
+        
+//         let modalHeader = document.createElement('h2');
+//         modalHeader.innerText = pokedexEntry.name;
+
+//         let modalButton = document.createElement('button');
+//         modalButton.classList.add('modal-button');
+//         modalButton.innerText = 'close';
+//         modalButton.addEventListener('click', hideModal);
+
+//         let modalContent = document.createElement('div');
+//         modalContent.classList.add('modal-content');
+//         let pokemonListTypes = getTypes(pokedexEntry.types);
+//         modalContent.innerHTML = 
+//         `
+//             <p><strong>Height:</strong> ${pokedexEntry.height}m.</p>
+//             <p><strong>Types:</strong> ${pokemonListTypes}
+//         `
+//         modal.appendChild(modalImage);
+//         modalInteriorDiv.appendChild(modalHeader);
+//         modalInteriorDiv.appendChild(modalButton);
+//         modalInteriorDiv.appendChild(modalContent);
+//         modal.appendChild(modalInteriorDiv);
+//         modalContainer.appendChild(modal);
+//         document.getElementsByTagName('body')[0].appendChild(modalContainer);
+        
+//         window.addEventListener('keydown', (e) => {
+//             if (e.key === 'Escape' && modalContainer) {
+//                 hideModal();
+//             }
+//         })
+
+//         modalContainer.addEventListener('click', (e) => {
+//             let target = e.target;
+//             if (target === modalContainer) {
+//                 hideModal();
+//             }
+//         })
+
+//         function hideModal() {
+//             modalContainer.remove();
+//         }
+
+//         function getTypes(pokedexTypes) {
+//             let types = '';
+//             let c = 1;
+//             pokedexTypes.forEach(function (typeEntry){
+//                 console.log(pokedexTypes);
+//                 console.log(c);
+//                 if (parseInt(pokedexTypes.length) === c) {
+//                     types += typeEntry.type.name;
+//                 } else {
+//                     types += `${typeEntry.type.name}, `;
+//                     c += 1;
+//                 }
+//             })
+//             return types;
+//         }
+//     });
+// },
+// addListItem: function(pokedexEntry) {
+//     let unorderedListItem = document.createElement('li');
+//     unorderedListItem.classList.add('pokemon-list__item');
+
+//     let unorderedListButton = document.createElement('button');
+//     unorderedListButton.classList.add('pokemon-list__button');
+
+//     unorderedListButton.addEventListener('click', function (event) {
+//         pokemonRepository.showDetails(pokedexEntry);
+//     });
+
+//     let buttonDiv = document.createElement('div');
+//     buttonDiv.classList.add('button-header');
+
+//     let buttonDivP = document.createElement('h2');
+//     buttonDivP.innerText = pokedexEntry.name;
+
+//     let buttonDivMore = document.createElement('div');
+//     buttonDivMore.classList.add('pokemon-list__toggle');
+//     buttonDivMore.innerHTML = `<p>show more +</p>`;
+
+//     buttonDiv.appendChild(buttonDivP);
+//     buttonDiv.appendChild(buttonDivMore);
+
+//     unorderedListButton.appendChild(buttonDiv);
+
+//     unorderedListItem.appendChild(unorderedListButton);
+
+//     return unorderedListItem;
+// },
+
 /**======================
  *    Begin No. 1
  *========================**/
